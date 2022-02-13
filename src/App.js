9f4f08bacafbfa3174c9b3ce4b3288ba94cd80b8
jelelahmed <you@example.com>
2022-02-13 22:19:11 +0100

    adding starting project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			string: 'hello Jelel'
+		}
+	}
+
+	render () {
+		return (
+			<div>
+				<h2>Hello World</h2>
+    	</div>
+		)
+	}
+}
+
+
+export default App;
