da5cf9e83854454d2288be525d0e79e62c150ff3
jelelahmed <you@example.com>
2022-02-26 01:38:33 +0100

    adding sign-in page, and form-input, sign-in and signOut component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
+import signInAndSignUpPage from './pages/signIn-signOut/signIn-signOut.component';
+
 import Header from './component/header/header-component';
 
 
@@ -20,6 +23,7 @@ const App = () =>  {
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route excat path='/shop' component={ShopPage} />
+					<Route excat path='/signin' component={signInAndSignUpPage} />
 				</Switch>
 			</div>
 		)
---
+++ src/pages/signIn-signOut/signIn-signOut.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SignIn from '../../component/sign-in/sign-in.component';
+
+import './signIn-signOut.styles.scss';
+
+const signInAndSignUpPage = () => (
+	<SignIn />
+);
+
+export default signInAndSignUpPage;
