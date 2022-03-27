7533e96bed90a2d9207fbf319af63b705e3a1376
jelelahmed <you@example.com>
2022-03-27 01:05:23 +0100

    adding shop collection into redux, and using reselect to pass props to shop component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,7 +4,8 @@ import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducers";
-// import directoryReducer from "./directory/directory.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 
 const persistConfig = {
@@ -15,7 +16,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
 	user: userReducer,
-	cart: cartReducer
+	cart: cartReducer,
+	directory: directoryReducer,
+	shop: shopReducer
 })
 
 export default persistReducer(persistConfig, rootReducer);
